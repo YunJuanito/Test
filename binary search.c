3dd18d728dbdd7cb633f25d471a585241565e8c2
Yunias Tegar Juanito <99226918+YunJuanito@users.noreply.github.com>
2022-02-15 19:44:31 +0900

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 int main()
-
 {
-int array[100];
-int search, c, n;
+  int c, first, last, middle, n, search, array[100];
 
-  printf("\n\n");
-  printf("Jumlah Elemen Array : ");
+  printf("Masukan Jumlah Elemen : ");
   scanf("%d", &n);
 
-  printf("Masukan %d Integer \n", n);
+  printf("Masuka %d Integer : \n", n);
 
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
 
-  printf("Masukan Integer yang akan anda cari : ");
+  printf("Masukan angka yang akan ditemukan\n");
   scanf("%d", &search);
 
-  for (c = 0; c < n; c++)
-  {
-    if (array[c] == search)    /* If required element is found */
-    {
-      printf("\n\n%d ada pada baris ke %d.\n", search, c+1);
+  first = 0;
+  last = n - 1;
+  middle = (first+last)/2;
+
+  while (first <= last) {
+    if (array[middle] < search)
+      first = middle + 1;
+    else if (array[middle] == search) {
+      printf("%d ada pada barisan ke %d.\n", search, middle+1);
       break;
     }
+    else
+      last = middle - 1;
+
+    middle = (first + last)/2;
   }
-  if (c == n)
-    printf("\n\n%d tidak dapat ditemukan.\n", search);
-    return 0;
+  if (first > last)
+    printf("Tidak dapat menemukan, %d tidak ada dalam data\n", search);
+
+  return 0;
 }
 
-// Pencarian linier mengambil setiap elemen array satu per satu dalam urutan logis dan memeriksa keberadaan elemen yang diinginkan.
-// Jika Anda mengakses semua item dan tidak dapat menemukan item yang Anda cari, pencarian akan gagal.
-// Dalam kasus terburuk, jumlah rata-rata kasus yang dipindai adalah setengah dari ukuran array (n / 2).
-// Oleh karena itu, pencarian linier dapat didefinisikan sebagai metode melintasi array untuk menemukan elemen tertentu.
-
+//Pencarian biner adalah algoritma yang sangat efisien.
+//Teknik pencarian ini mengurangi waktu yang dihabiskan untuk mencari item tertentu dengan perbandingan minimal.
+//Untuk melakukan pencarian biner, Anda harus terlebih dahulu mengurutkan elemen array.
